6d0fb68ed9c8593bf19008f49a445173f0d4f250
Hard4 <hard4y@gmail.com>
2015-07-11 11:33:46 +0600

    #ico + beta v0.98

---
+++ ZIAS/build/resource3.h
@@ -0,0 +1,11 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ZIAS3.rc
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define IDI_APPICON                     101
+#endif
+#endif
